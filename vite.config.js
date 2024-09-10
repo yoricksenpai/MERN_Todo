e0391653474282b208442b2795809e356bbe7145
Johan Priso <116643216+yoricksenpai@users.noreply.github.com>
2024-09-10 17:16:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isProd = import.meta.env.PROD // Vite fournit PROD, qui est true en production
-const API_URL = isProd 
-  ? 'https://mern-todo-backend-nine-sigma.vercel.app' 
-  : 'http://localhost:3000'
-
+const API_URL ='https://mern-todo-backend-nine-sigma.vercel.app' 
+ 
 
 export default defineConfig({
   plugins: [react()],
