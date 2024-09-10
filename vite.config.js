ba96f2c9b9a0cccbfd14a9192328585def16b328
Johan Priso <116643216+yoricksenpai@users.noreply.github.com>
2024-09-10 15:43:49 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       server: {
       proxy: {
         '/api': {
-          target:targetUrl,
+          target:'https://mern-todo-backend-nine-sigma.vercel.app/',
               changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '')
         }
