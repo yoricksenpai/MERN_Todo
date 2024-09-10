975f2ba6a6fced54ebfab0570d42f3149c42e1a2
Johan Priso <116643216+yoricksenpai@users.noreply.github.com>
2024-09-10 17:14:19 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {env} from 'node:process'
 
-const isProd = env.NODE_ENV === 'production'
-const targetUrl = isProd
-  ? 'https://mern-todo-backend-nine-sigma.vercel.app/'
-  : 'http://localhost:5173/'
+const isProd = import.meta.env.PROD // Vite fournit PROD, qui est true en production
+const API_URL = isProd 
+  ? 'https://mern-todo-backend-nine-sigma.vercel.app' 
+  : 'http://localhost:3000'
+
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: targetUrl,
+        target: API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
