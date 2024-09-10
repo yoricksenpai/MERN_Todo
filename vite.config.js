8d9a59db8b2dc5f80afb5a1d956e194885813176
Johan Priso <116643216+yoricksenpai@users.noreply.github.com>
2024-09-10 16:44:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import {env} from 'node:process'
 
-const targetUrl = env.NODE_ENV === 'production'
+const isProd = env.NODE_ENV === 'production'
+const targetUrl = isProd
   ? 'https://mern-todo-backend-nine-sigma.vercel.app/'
-  : 'http://localhost:3000/'
+  : 'http://localhost:5173/'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-      server: {
-      proxy: {
-        '/api': {
-          target:'https://mern-todo-backend-nine-sigma.vercel.app/',
-              changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
+  server: {
+    proxy: {
+      '/api': {
+        target: targetUrl,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
+  }
 })
