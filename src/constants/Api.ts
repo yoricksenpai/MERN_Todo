fff34a0a94ad691d3d9c89e4f707ffdb69997ee3
Johan Priso <prisojohan2@gmail.com>
2025-09-07 08:18:58 +0100

    Slash dumbass error

--- src/constants/Api.ts
+++ src/constants/Api.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
 const isProd = import.meta.env.PROD;
-const API_URL: string = isProd ? 'https://todo-backend-two-sandy.vercel.app/' : 'http://localhost:3000';
+const API_URL: string = isProd ? 'https://todo-backend-two-sandy.vercel.app' : 'http://localhost:3000';
 
 export default API_URL;
